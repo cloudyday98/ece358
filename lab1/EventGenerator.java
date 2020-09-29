123b234816991bf67fcd0edbbb76eabe082bb11d
y76ye <y76ye@uwaterloo.ca>
2020-09-29 16:05:12 -0400

    lab 1 finished

--- lab1/EventGenerator.java
+++ lab1/EventGenerator.java
@@ -1,7 +1,12 @@
 package lab1;
-
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.ArrayList;
 
 public class EventGenerator {
+	
+	private static int event_dropped = 0;
 	
 	public static EventQueue GenerateArrivalEvents(EventQueue event_queue, 
 			double lambda, int simulation_time) {
@@ -45,16 +50,80 @@ public class EventGenerator {
 		
 		return new_event_queue; 
 	}
-	
+
 	public static EventQueue GenerateDepartureEventsForMM1K(EventQueue event_queue, 
-			int packet_length,
-			int transmission_rate) {
+			int average_packet_length,
+			int transmission_rate,
+			int max_queue_size) {
 		
-		//TODO: generate the departure events for MM1K queue
+		event_dropped = 0;
 		
-		return new EventQueue(); 
+		EventQueue new_event_queue = new EventQueue(event_queue);
+		PriorityQueue<Event> temp = event_queue.GetEventQueue();
+		ArrayList<Event> departure_pending_list = new ArrayList<>();
+		double departure_time = 0.0;
+		Event current_event;
+		
+		while(!temp.isEmpty()) {
+			current_event = temp.poll();
+			
+			if (departure_pending_list.isEmpty()) {
+				departure_time = current_event.GetEventTime() + 
+						RandomVariableGenerator.GenerateRandomVariable(1/(double)average_packet_length)/transmission_rate;
+				departure_pending_list.add(new Event(departure_time, Event.EventType.DEPARTURE));
+			}
+			
+			else {
+				
+				// remove all the packets that can depart before the next arrival event
+				while (!departure_pending_list.isEmpty()) {
+					
+					if (departure_pending_list.get(0).GetEventTime() < current_event.GetEventTime()) {
+						new_event_queue.AddToEventQueue(departure_pending_list.get(0));
+						departure_pending_list.remove(0);
+					}
+					
+					else {
+						break;
+					}
+					
+				}
+				
+				// if queue is full, the  drop the event
+				if (departure_pending_list.size() >= max_queue_size) {
+					event_dropped ++;
+				}
+				
+				else {
+					
+					if (departure_pending_list.isEmpty()) {
+						departure_time = current_event.GetEventTime() + 
+								RandomVariableGenerator.GenerateRandomVariable(1/(double)average_packet_length)/transmission_rate;
+						departure_pending_list.add(new Event(departure_time, Event.EventType.DEPARTURE));
+					}
+					
+					else {
+						departure_time = departure_pending_list.get(departure_pending_list.size() - 1).GetEventTime()
+								+ RandomVariableGenerator.GenerateRandomVariable(1/(double)average_packet_length)/transmission_rate;
+						departure_pending_list.add(new Event(departure_time, Event.EventType.DEPARTURE));
+					}
+
+				}
+			}
+
+		}
+		
+		for (Event e : departure_pending_list) {
+			new_event_queue.AddToEventQueue(e);
+		}
+	
+		return new_event_queue;
 	}
 	
+	public static int GetPacketLoss(){
+		return event_dropped;
+	}
+
 	public static EventQueue GenerateObserverEvents
 	(EventQueue event_queue, double lambda, int simulation_time) {
 		
--- lab1/MM1KQueueSimulator.java
+++ lab1/MM1KQueueSimulator.java
@@ -1,5 +1,97 @@
 package lab1;
 
 public class MM1KQueueSimulator {
+	
+	// parameters
+	private static final int L = 2000; //average packet length
+	private static final int C = 1000000;
+	private static final int T = 1000;
+	private static final int[] K = {10, 25, 50};
+	private static double RHO = 0.5;
+	private static double lambda;
+	private static int packet_loss;
+	
+	private static EventQueue event_queue;
+	
+	public static void main(String[] args) {
 
+		RunSimulation();
+		
+	}
+	
+	public static void RunSimulation() {
+		
+		event_queue = new EventQueue();
+		
+		// Testing on RHO is from 0.25 to 0.95
+		
+		while (RHO <= 1.51) {
+			
+			lambda = RHO*C/L;
+			
+			for (int k : K) {
+				event_queue = EventGenerator.GenerateArrivalEvents(event_queue, lambda, T);
+
+				event_queue = EventGenerator.GenerateDepartureEventsForMM1K(event_queue, L, C, k);
+				
+				packet_loss = EventGenerator.GetPacketLoss();
+				
+				event_queue = EventGenerator.GenerateObserverEvents(event_queue, lambda, T);
+				
+				ProcessEvents(k);
+			}
+			
+			RHO += 0.1;
+		}
+		
+		
+	}
+	
+	public static void ProcessEvents(int buffer_size) {
+		
+		Event current_event;
+		int queue_size = 0;
+		int arrival_count = 0;
+		long queue_size_sum = 0;
+		int observer_count = 0;
+		
+		while(event_queue.GetQueueSize() != 0) {
+			current_event = event_queue.RemoveEvent();
+			
+			switch (current_event.GetEventType()) {
+				
+				case ARRIVAL:
+					
+					arrival_count += 1;
+					
+					if (queue_size < buffer_size) {
+						queue_size += 1;
+					}
+					
+					break;
+					
+				case DEPARTURE:
+					
+					queue_size -= 1;
+					
+					
+					break;
+					
+				case OBSERVER:
+					
+					observer_count += 1;
+					queue_size_sum += queue_size;
+					
+					break;
+			}
+		}
+		
+		double EN = (double)queue_size_sum/(double)observer_count;
+		System.out.println("RHO = " + RHO + ", K = " + buffer_size);
+		System.out.println("Average Number of Packet: " + EN);
+		System.out.println("Percentage of Loss: " + (double)packet_loss/(double)arrival_count);
+		System.out.println("");
+		
+		
+	}
 }
